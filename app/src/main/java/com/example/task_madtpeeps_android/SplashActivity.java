8ce3adf31177392e4204c57b0126ec7e2f81ed9e
surabhirupani <c0829588@mylambton.ca>
2022-02-02 19:36:25 -0500

    adding animation to splash screen

--- app/src/main/java/com/example/task_madtpeeps_android/SplashActivity.java
+++ app/src/main/java/com/example/task_madtpeeps_android/SplashActivity.java
@@ -2,13 +2,53 @@ package com.example.task_madtpeeps_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+    TextView tvTitle;
+    int SPLASH_TIME_OUT = 6000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        tvTitle = findViewById(R.id.tvTitle);
+
+       setAnimation();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    private void setAnimation() {
+        ObjectAnimator scaleXAnimation = ObjectAnimator.ofFloat(tvTitle, "scaleX", 5.0F, 1.0F);
+        scaleXAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+        scaleXAnimation.setDuration(2000);
+        ObjectAnimator scaleYAnimation = ObjectAnimator.ofFloat(tvTitle, "scaleY", 5.0F, 1.0F);
+        scaleYAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+        scaleYAnimation.setDuration(2000);
+        ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(tvTitle, "alpha", 0.0F, 1.0F);
+        alphaAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+        alphaAnimation.setDuration(2000);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(scaleXAnimation).with(scaleYAnimation).with(alphaAnimation);
+        animatorSet.setStartDelay(2000);
+        animatorSet.start();
     }
 }
