3915688eeff0b73a7d1e17f1f04f2033a5392476
c0832707 <97565518+Asky707@users.noreply.github.com>
2022-02-07 15:27:38 -0500

    create interface dao

--- app/src/main/java/com/example/task_madtpeeps_android/Database/AppDatabase.java
+++ app/src/main/java/com/example/task_madtpeeps_android/Database/AppDatabase.java
@@ -10,8 +10,10 @@ import androidx.room.TypeConverters;
 import com.example.task_madtpeeps_android.Model.User;
 
 @Database(entities = {User.class}, version = 2, exportSchema = false)
-
+@TypeConverters({com.example.task_madtpeeps_android.Database.TypeConverters.class})
 public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract DAO getDAO();
 
     private static AppDatabase INSTANCE;
 
