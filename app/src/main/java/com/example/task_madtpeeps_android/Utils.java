5bf3aadf48c2aec0d7b09e5839dad8f92e7f3fef
c0832707 <97565518+Asky707@users.noreply.github.com>
2022-02-08 17:57:22 -0500

    create utils class for handling keys of session

---
+++ app/src/main/java/com/example/task_madtpeeps_android/Utils.java
@@ -0,0 +1,11 @@
+package com.example.task_madtpeeps_android;
+
+public class Utils {
+
+    public static final String APP_NAME = "TASKMANAGER";
+
+    //Login Utils
+    public static final String loginControlKey = "isLogin";
+    public static final String loginUserNameKey = "userName";
+    public static final String loginUserPassword = "userPassword";
+}
