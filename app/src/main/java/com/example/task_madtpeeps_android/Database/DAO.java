9351706173d822e6c1241a3c73182f1677bf6dae
C0817241 <97565515+C0817241@users.noreply.github.com>
2022-02-09 20:05:19 -0500

    create methods add edit delete task

--- app/src/main/java/com/example/task_madtpeeps_android/Database/DAO.java
+++ app/src/main/java/com/example/task_madtpeeps_android/Database/DAO.java
@@ -6,6 +6,7 @@ import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import com.example.task_madtpeeps_android.Model.Category;
+import com.example.task_madtpeeps_android.Model.Task;
 import com.example.task_madtpeeps_android.Model.User;
 
 import java.util.List;
@@ -19,9 +20,18 @@ public interface DAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertCategory(Category category);
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertTask(Task task);
+
     //Delete Querys
     @Query("DELETE FROM category WHERE categoryId = :categoryId")
     void deleteCategory(Long categoryId);
+
+    @Query("DELETE FROM task WHERE categoryId = :categoryId")
+    void deleteTaskByCategory(Long categoryId);
+
+    @Query("DELETE FROM task WHERE taskId = :taskId")
+    void deleteTask(Long taskId);
 
     //Select Querys
     @Query("SELECT * FROM user WHERE userName = :userName AND userPassword = :password")
@@ -35,4 +45,22 @@ public interface DAO {
 
     @Query("SELECT category.* FROM category WHERE userId = :userId")
     List<Category> getCategorylist(String userId);
+
+    @Query("SELECT COUNT(task.categoryId) FROM category " +
+            " LEFT JOIN task ON task.categoryId = category.categoryId " +
+            " WHERE category.userId = :userId AND CASE :countType " +
+            "WHEN '0' THEN task.taskStatusCode = 0 " +
+            "WHEN '1' THEN task.taskStatusCode = 1 " +
+            "WHEN '-1' THEN task.taskDeadline > :expiry " +
+            "END")
+    int getTaskCount(Long userId, String countType, String expiry);
+
+    @Query("SELECT * FROM task WHERE taskId = :taskId")
+    List<Task> getTasks(String taskId);
+    
+    @Query("SELECT * FROM task WHERE categoryId = :categoryId ORDER BY CASE :orderType " +
+            "WHEN 'taskCreateDate' THEN taskCreateDate " +
+            "WHEN 'taskName' THEN taskName " +
+            "END ASC")
+    List<Task> getTaskByOrder(String categoryId, String orderType);
 }
