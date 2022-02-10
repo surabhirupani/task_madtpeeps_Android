33f61d1056078ff5df7af4014443c1bb834fd144
C0817241 <97565515+C0817241@users.noreply.github.com>
2022-02-09 19:39:53 -0500

    create task class and add attributes to it

---
+++ app/src/main/java/com/example/task_madtpeeps_android/Model/Task.java
@@ -0,0 +1,113 @@
+package com.example.task_madtpeeps_android.Model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "task")
+public class Task {
+    @PrimaryKey
+    private Long taskId;
+
+    @ColumnInfo
+    private String categoryId;
+
+    @ColumnInfo
+    private String taskName;
+
+    @ColumnInfo
+    private String taskDesc;
+
+    @ColumnInfo
+    private Date taskDeadline;
+
+    @ColumnInfo
+    private Date taskCreateDate;
+
+    @ColumnInfo
+    private int taskStatusCode;
+
+    @ColumnInfo
+    private String taskStatusName;
+
+    private boolean isExpanded;//Definition For Expandable RecyclerView
+
+    public Task() {
+        isExpanded = false;
+        taskStatusCode = 0;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDesc() {
+        return taskDesc;
+    }
+
+    public void setTaskDesc(String taskDesc) {
+        this.taskDesc = taskDesc;
+    }
+
+    public Date getTaskDeadline() {
+        return taskDeadline;
+    }
+
+    public void setTaskDeadline(Date taskDeadline) {
+        this.taskDeadline = taskDeadline;
+    }
+
+    public Date getTaskCreateDate() {
+        return taskCreateDate;
+    }
+
+    public void setTaskCreateDate(Date taskCreateDate) {
+        this.taskCreateDate = taskCreateDate;
+    }
+
+    public int getTaskStatusCode() {
+        return taskStatusCode;
+    }
+
+    public void setTaskStatusCode(int taskStatusCode) {
+        this.taskStatusCode = taskStatusCode;
+    }
+
+    public String getTaskStatusName() {
+        return taskStatusName;
+    }
+
+    public void setTaskStatusName(String taskStatusName) {
+        this.taskStatusName = taskStatusName;
+    }
+
+    public boolean isExpanded() {
+        return isExpanded;
+    }
+
+    public void setExpanded(boolean expanded) {
+        isExpanded = expanded;
+    }
+}
